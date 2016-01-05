d1d39989c1d242a5e098fa99bb2966b20a2c5939
StickyBros <admin@StickyBros.org>
2016-01-05 16:17:42 -0500

    Experimenting with errbit

--- src/client/main.js
+++ src/client/main.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import Router from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import createRoutes from './createRoutes';
+//import airbrakeJs from 'airbrake-js';
+
+const AirbrakeClient = require('airbrake-js');
+
+const airbrake = new AirbrakeClient();
+airbrake.setHost('http://192.168.99.100:5000');
+airbrake.setProject('105cd6c8fb856a26f8ab0fa2f866337a', '105cd6c8fb856a26f8ab0fa2f866337a');
+
+airbrake.notify({error: {message: 'test'}});
 
 const app = document.getElementById('app');
 
