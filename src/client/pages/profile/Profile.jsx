316fe85dcb4c44263bd693ec998b5a65a620bb51
oneMore Admin <admin@onemore.rocks>
2016-03-05 11:59:19 -0500

    Made measures more accurate by translating between units properly.

--- src/client/pages/profile/Profile.jsx
+++ src/client/pages/profile/Profile.jsx
@@ -28,8 +28,7 @@ for (let i = 22; i <= 226; i += 2) {
 
 const lbs = [];
 for (let i = 50; i <= 500; i += 5) {
-  const rawKg = Math.round(i * 0.453592);
-  const kg = rawKg % 2 === 0 ? rawKg : rawKg - 1;
+  const kg = Math.round(i * 0.453592 * 100) / 100;
   lbs.push({ label: i + ' lb', value: kg });
 }
 
@@ -37,26 +36,48 @@ const feet = [];
 for (let i = 48; i <= 96; i++) {
   const foot = Math.floor(i / 12);
   const inches = i - foot * 12;
-  const cm = Math.round(i * 2.54);
+  const cm = Math.round(i * 2.54 * 100) / 100;
   feet.push({ label: foot + '\'' + inches + '"', value: cm });
 }
 
 const cms = [];
 feet.forEach((foot) => {
-  cms.push({ label: foot.value + ' cm', value: foot.value });
+  const roundedCm = Math.round(foot.value);
+  cms.push({ label: roundedCm + ' cm', value: roundedCm });
 });
 
 const waistInches = [];
 for (let i = 20; i <= 80; i += 2) {
-  const cm = Math.round(i * 2.54);
-  waistInches.push({ label: i + '', value: cm });
+  const cm = Math.round(i * 2.54 * 100) / 100;
+  waistInches.push({ label: i + '"', value: cm });
 }
 
 const waistCms = [];
-waistInches.forEach((waistInch) => {
-  waistCms.push({ label: waistInch.value + ' cm', value: waistInch.value });
-});
+for (let i = 50; i <= 200; i += 5) {
+  waistCms.push({ label: i + ' cm', value: i });
+}
 
+const cockLengthInches = [];
+for (let i = 2; i <= 12; i += 0.5) {
+  const cm = Math.round(i * 2.54 * 100) / 100;
+  cockLengthInches.push({ label: i + '"', value: cm });
+}
+
+const cockLengthCms = [];
+for (let i = 5; i <= 30; i += 1) {
+  cockLengthCms.push({ label: i + ' cm', value: i });
+}
+
+const cockGirthInches = [];
+for (let i = 2; i <= 8; i += 0.25) {
+  const cm = Math.round(i * 2.54 * 100) / 100;
+  cockGirthInches.push({ label: i + '"', value: cm });
+}
+
+const cockGirthCms = [];
+for (let i = 5; i <= 20; i += 1) {
+  cockGirthCms.push({ label: i + ' cm', value: i });
+}
 
 const decorateLessMore = (array) => {
   array[0].label = array[0].label + ' or less';
@@ -67,6 +88,8 @@ decorateLessMore(kgs);
 decorateLessMore(lbs);
 decorateLessMore(feet);
 decorateLessMore(cms);
+decorateLessMore(waistInches);
+decorateLessMore(waistCms);
 
 const FormGroup = (props) =>
   <div className="small-12 medium-6 large-4 columns end">
@@ -155,16 +178,66 @@ class Profile extends Component {
     };
   };
 
+  _closest(num, arr) {
+    var mid;
+    var lo = 0;
+    var hi = arr.length - 1;
+    while (hi - lo > 1) {
+      mid = Math.floor((lo + hi) / 2);
+      if (arr[mid] < num) {
+        lo = mid;
+      } else {
+        hi = mid;
+      }
+    }
+    if (num - arr[lo] <= arr[hi] - num) {
+      return arr[lo];
+    }
+    return arr[hi];
+  }
+
+  _handleOnUnitChange = (key, mapping) => {
+    const changeFunc = this._handleOnChange(key);
+    return (newUnit) => {
+      mapping.targets.forEach((target, i) => {
+
+        const values = mapping[newUnit][i].map((entry) => {
+          return entry.value;
+        });
+
+        const previousValue = this.state[target];
+        if (previousValue) {
+          const newValue = this._closest(previousValue, values);
+          const stateObj = {};
+          stateObj[target] = newValue;
+          this.setState(stateObj);
+        }
+      });
+      changeFunc(newUnit);
+    };
+  };
+
   _handleNickname = this._handleOnChange('nickname');
   _handleBirthYear = this._handleOnChange('birthYear');
   _handleBirthMonth = this._handleOnChange('birthMonth');
-  _handleWeightUnits = this._handleOnChange('weightUnits');
+  _handleWeightUnits = this._handleOnUnitChange('weightUnits', {
+    lb: [lbs], kg: [kgs], targets: ['weight']
+  });
   _handleWeight = this._handleOnChange('weight');
-  _handleHeightUnits = this._handleOnChange('heightUnits');
+  _handleHeightUnits = this._handleOnUnitChange('heightUnits', {
+    feet: [feet], cm: [cms], targets: ['height']
+  });
   _handleHeight = this._handleOnChange('height');
-  _handleWaistUnits = this._handleOnChange('waistUnits');
+  _handleWaistUnits = this._handleOnUnitChange('waistUnits', {
+    inches: [waistInches], cm: [waistCms], targets: ['waist']
+  });
   _handleWaist = this._handleOnChange('waist');
   _handleForeskin = this._handleOnChange('foreskin');
+  _handleCockUnits = this._handleOnUnitChange('cockUnits', {
+    inches: [cockLengthInches, cockGirthInches], cm: [cockLengthCms, cockGirthCms], targets: ['cockLength', 'cockGirth']
+  });
+  _handleCockLength = this._handleOnChange('cockLength');
+  _handleCockGirth = this._handleOnChange('cockGirth');
 
   render() {
     const profile = this.props.login.profile;
@@ -221,7 +294,7 @@ class Profile extends Component {
                 <input type="text" value={state.nickname} maxLength="20" required onChange={this._handleNickname} />
               </label>
             </div>
-            <div className="small-6 large-4 columns">
+            <div className="small-6 large-4 columns profile-inputbox">
               <label>
                 Birth Year
                 <select required defaultValue="" value={state.birthYear} onChange={this._handleBirthYear}>
@@ -232,7 +305,7 @@ class Profile extends Component {
                 </select>
               </label>
             </div>
-            <div className="small-6 large-4 columns">
+            <div className="small-6 large-4 columns profile-inputbox">
               <label>
                 Birth Month
                 <select required defaultValue="" value={state.birthMonth} onChange={this._handleBirthMonth}>
@@ -243,7 +316,7 @@ class Profile extends Component {
                 </select>
               </label>
             </div>
-            <div className="small-12 medium-6 large-4 columns">
+            <div className="small-12 medium-6 large-4 columns profile-inputbox">
               <div>
                 <label className="hor-label-container" htmlFor="weight">
                   Weight
@@ -269,7 +342,7 @@ class Profile extends Component {
                 </select>
               </div>
             </div>
-            <div className="small-12 medium-6 large-4 columns">
+            <div className="small-12 medium-6 large-4 columns profile-inputbox">
               <div>
                 <label className="hor-label-container" htmlFor="height">
                   Height
@@ -294,40 +367,80 @@ class Profile extends Component {
                 </select>
               </div>
             </div>
-            <div className="small-12 medium-6 large-4 columns">
-              <div>
-                <label className="hor-label-container" htmlFor="waist">
-                  Waist
+            <div className="small-12 medium-6 large-4 columns profile-inputbox">
+              <label className="hor-label-container" htmlFor="waist">
+                Waist
+              </label>
+              <RadioGroup name="waistUnits" value={state.waistUnits} onChange={this._handleWaistUnits} className="hor-label-container">
+                <label className="hor-label">
+                  <input className="hor-input" type="radio" value="inches" required />inches
                 </label>
-                <RadioGroup name="waistUnits" value={state.waistUnits} onChange={this._handleWaistUnits} className="hor-label-container">
-                  <label className="hor-label">
-                    <input className="hor-input" type="radio" value="inches" required />inches
-                  </label>
-                  <label className="hor-label">
-                    <input className="hor-input" type="radio" value="cm" />cm
-                  </label>
-                </RadioGroup>
-                <select id="waist" defaultValue="" value={state.waist} onChange={this._handleWaist}>
-                  {!state.waist && <option disabled hidden value="" />}
-                  {state.waistUnits === 'inches' && waistInches.map((inch) => {
-                    return <option key={'waistInches' + inch.value} value={inch.value}>{inch.label}</option>;
-                  })}
-                  {state.waistUnits === 'cm' && waistCms.map((cm) => {
-                    return <option key={'waistCms' + cm.value} value={cm.value}>{cm.label}</option>;
-                  })}
-                </select>
-              </div>
+                <label className="hor-label">
+                  <input className="hor-input" type="radio" value="cm" />cm
+                </label>
+              </RadioGroup>
+              <select id="waist" defaultValue="" value={state.waist} onChange={this._handleWaist}>
+                {!state.waist && <option disabled hidden value="" />}
+                {state.waistUnits === 'inches' && waistInches.map((inch) => {
+                  return <option key={'waistInches' + inch.value} value={inch.value}>{inch.label}</option>;
+                })}
+                {state.waistUnits === 'cm' && waistCms.map((cm) => {
+                  return <option key={'waistCms' + cm.value} value={cm.value}>{cm.label}</option>;
+                })}
+              </select>
             </div>
-            <div className="small-6 large-4 columns">
+            <div className="small-12 medium-6 large-4 columns profile-inputbox">
               <label>
                 Foreskin
                 <select required defaultValue="" value={state.foreskin} onChange={this._handleForeskin}>
                   {!state.foreskin && <option disabled hidden value="" />}
                   <option value="cut">Cut</option>
+                  <option value="semicut">Semi-Cut</option>
                   <option value="uncut">Uncut</option>
                 </select>
               </label>
             </div>
+            <div className="small-12 medium-6 large-4 columns profile-inputbox">
+              <label className="hor-label-container" htmlFor="cockLength">
+                Cock Length
+              </label>
+              <RadioGroup name="cockUnits" value={state.cockUnits} onChange={this._handleCockUnits} className="hor-label-container">
+                <label className="hor-label">
+                  <input className="hor-input" type="radio" value="inches" required />inches
+                </label>
+                <label className="hor-label">
+                  <input className="hor-input" type="radio" value="cm" />cm
+                </label>
+              </RadioGroup>
+              <select id="cockLength" defaultValue="" value={state.cockLength} onChange={this._handleCockLength}>
+                {!state.cockLength && <option disabled hidden value="" />}
+                {state.cockUnits === 'inches' && cockLengthInches.map((inch) => {
+                  return <option key={'cockLengthInches' + inch.value} value={inch.value}>{inch.label}</option>;
+                })}
+                {state.cockUnits === 'cm' && cockLengthCms.map((cm) => {
+                  return <option key={'cockLengthCms' + cm.value} value={cm.value}>{cm.label}</option>;
+                })}
+              </select>
+            </div>
+            <div className="small-12 medium-6 large-4 columns profile-inputbox">
+              <label className="hor-label-container" htmlFor="cockGirth">
+                Cock Girth
+              </label>
+              <select id="cockGirth" defaultValue="" value={state.cockGirth} onChange={this._handleCockGirth}>
+                {!state.cockGirth && <option disabled hidden value="" />}
+                {state.cockUnits === 'inches' && cockGirthInches.map((inch) => {
+                  return <option key={'cockGirthInches' + inch.value} value={inch.value}>{inch.label}</option>;
+                })}
+                {state.cockUnits === 'cm' && cockGirthCms.map((cm) => {
+                  return <option key={'cockGirthCms' + cm.value} value={cm.value}>{cm.label}</option>;
+                })}
+              </select>
+            </div>
+            <div className="small-12 medium-6 large-4 columns profile-inputbox">
+            </div>
+
+          </div>
+          <div className="row">
             <div className="small-12 columns">
               <h3>Vanilla Sex</h3>
             </div>
